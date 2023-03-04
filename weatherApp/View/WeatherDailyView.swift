cf9b91e567ee16ac2afb16014154e57e4599d25c
FACON-Nicolas <faconicolas@gmail.com>
2023-03-04 17:55:03 +0000

    transform icon in a View

---
+++ weatherApp/View/WeatherDailyView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherDailyView.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 04/03/2023.
+//
+
+import SwiftUI
+
+struct WeatherDailyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherDailyView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherDailyView()
+    }
+}
---
+++ weatherApp/View/WeatherIconView.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherIconView.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 04/03/2023.
+//
+
+import SwiftUI
+
+struct WeatherIconView: View {
+    
+    var name: String
+    
+    var body: some View {
+        Image(systemName: name)
+            .symbolRenderingMode(.multicolor)
+            .font(.system(size: 25))
+            .padding()
+    }
+}
+
+struct WeatherIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherIconView(name: "cloud.sun.rain.fill")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
