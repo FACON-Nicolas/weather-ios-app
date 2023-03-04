043b62c731fe3adb79524965220f55289c2a0b1d
FACON-Nicolas <faconicolas@gmail.com>
2023-03-04 15:49:48 +0000

    initial commit

---
+++ weatherApp/View/WeatherHourlyView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherHourlyView.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 04/03/2023.
+//
+
+import SwiftUI
+
+struct WeatherHourlyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherHourlyView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherHourlyView()
+    }
+}
---
+++ weatherApp/View/WeatherLocationView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherLocationView.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 04/03/2023.
+//
+
+import SwiftUI
+
+struct WeatherLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherLocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherLocationView()
+    }
+}
