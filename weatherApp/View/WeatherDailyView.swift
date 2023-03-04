4297c8afe14d2c29838704c3fa2b06cd0ffaaf41
FACON-Nicolas <faconicolas@gmail.com>
2023-03-04 17:56:22 +0000

    add Daily weather view

--- weatherApp/View/WeatherDailyView.swift
+++ weatherApp/View/WeatherDailyView.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
+
 struct WeatherDailyView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ForEach(0..<3) { item in
+                HStack {
+                    Text("coucou")
+                        .fontWeight(.bold)
+
+                    WeatherIconView(name: "sun.max.fill")
+                        .padding(.horizontal)
+                    
+                    Text("0°")
+                        .fontWeight(.bold)
+                    
+                    Text("12°")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+            }
+        }
+        .background(.black.opacity(0.05))
+        .cornerRadius(12)
+        .padding()
     }
 }
 
 struct WeatherDailyView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherDailyView()
+            .previewLayout(.sizeThatFits)
     }
 }
