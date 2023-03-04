e946d56af928486308524551cccbf4362bec8ed5
FACON-Nicolas <faconicolas@gmail.com>
2023-03-04 16:14:56 +0000

    add prototype for hour by hour temperature

--- weatherApp/View/WeatherHourlyView.swift
+++ weatherApp/View/WeatherHourlyView.swift
@@ -9,12 +9,34 @@ import SwiftUI
 
 struct WeatherHourlyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { item in
+                    VStack {
+                        Text("\(item)h")
+                            .fontWeight(.bold)
+                        
+                        Image(systemName: "cloud.sun.rain.fill")
+                            .shadow(radius: 3)
+                            .symbolRenderingMode(.multicolor)
+                            .font(.system(size: 25))
+                            .padding()
+                        
+                        Text("\(9)°")
+                    }
+                }
+            }
+        }
+        .padding(.vertical)
+        .background(.black.opacity(0.1))
+        .cornerRadius(12)
     }
 }
 
 struct WeatherHourlyView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherHourlyView()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
