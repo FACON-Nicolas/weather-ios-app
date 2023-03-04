8e4fa5a5b23c80d3cd9e43e8fdcf4d5ee23d6f62
FACON-Nicolas <faconicolas@gmail.com>
2023-03-04 15:08:33 +0000

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
