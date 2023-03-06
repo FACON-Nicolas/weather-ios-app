455e23f8c78655ce0e1600999d41ccee42d529db
FACON-Nicolas <faconicolas@gmail.com>
2023-03-06 19:17:00 +0000

    add model for weather

---
+++ weatherApp/Data/WeatherAPICall.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherAPICall.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 05/03/2023.
+//
+
+import Foundation
---
+++ weatherApp/Model/WeatherModel.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherModel.swift
+//  weatherApp
+//
+//  Created by Nicolas FACON on 05/03/2023.
+//
+
+import Foundation
+
+struct WeatherModel {
+    var id = UUID()
+    var city: String
+    var degrees : String
+    var min: String
+    var max: String
+    var weather: String
+}
+
+
