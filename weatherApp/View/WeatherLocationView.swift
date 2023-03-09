5c20ed1998f39d55476b9775fac24395fd01cb71
FACON-Nicolas <faconicolas@gmail.com>
2023-03-09 18:56:53 +0000

    useless commit

--- weatherApp/View/WeatherLocationView.swift
+++ weatherApp/View/WeatherLocationView.swift
@@ -5,11 +5,13 @@
 //  Created by Nicolas FACON on 04/03/2023.
 //
 
+import CoreLocation
 import SwiftUI
+import CoreLocation
 
 struct WeatherLocationView: View {
     
-    var city: String
+    @State var city: String = "Paris"
     @State var weather : WeatherModel = WeatherModel(city: "--", degrees: "", min: "", max: "", weather: "--")
     @State var week : WeatherWeek = WeatherWeek()
     @State var hours: WeatherHours = WeatherHours()
@@ -20,13 +22,14 @@ struct WeatherLocationView: View {
                 VStack {
                     Text(weather.city.capitalized)
                         .font(.system(size: 30))
-                        
+                    
                     Text(" \(weather.degrees)°")
                         .font(.system(size: 90))
                         .fontWeight(.light)
                     
                     Text(weather.weather)
                         .font(.system(size: 20))
+                        .multilineTextAlignment(.center)
                     
                     HStack {
                         Image(systemName: "arrow.up")
@@ -43,24 +46,28 @@ struct WeatherLocationView: View {
                     .padding(.horizontal)
                 
                 WeatherDailyView(week: week)
+                
+                
+                
             }
             .foregroundColor(.white)
+            .task {
+                weather = await WeatherData.fetchWeather(city: city)
+                weather.city = city
+                week = WeatherData.fetchWeek()
+                week.degrees = Double(weather.degrees)!
+                hours = WeatherData.fetchHours()
+            }
         }
         .background(LinearGradient(colors: WeatherData.getColorsName(weather: weather.weather),
-         startPoint: .top,
-         endPoint: .bottom))
-        .task {
-            weather = await WeatherData.fetchWeather(city: city)
-            week = WeatherData.fetchWeek()
-            week.degrees = Double(weather.degrees)!
-            hours = WeatherData.fetchHours()
-        }
+                                   startPoint: .top,
+                                   endPoint: .bottom))
     }
     
 }
 
 struct WeatherLocationView_Previews: PreviewProvider {
     static var previews:  some View {
-        WeatherLocationView(city: "lens")
+        WeatherLocationView()
     }
 }
