079e5daec8206462334c9f7d6c38abc023c44271
FACON-Nicolas <faconicolas@gmail.com>
2023-03-10 08:13:06 +0000

    add hours for days and check global weather (not opti)

--- weatherApp/Model/WeatherDaily.swift
+++ weatherApp/Model/WeatherDaily.swift
@@ -8,8 +8,34 @@
 import Foundation
 
 struct WeatherDaily: Identifiable, Equatable {
+    
+    static func == (lhs: WeatherDaily, rhs: WeatherDaily) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func weather() -> String {
+        var icons: [String] = []
+        
+        var iconsCount: [String:Int] = [:]
+        
+        for i in 0..<hours.hours.count {
+            var ic = WeatherData.getIconName(weather: hours.hours[i].weather)
+            icons.append(ic)
+            if !iconsCount.keys.contains(ic) {
+                iconsCount[ic] = 1
+            } else {
+                iconsCount[ic]! += 1
+            }
+        }
+        
+        let weather = iconsCount.max { $0.value < $1.value }
+        
+        return Array(iconsCount.keys)[0]
+    }
+    
     var id = UUID()
     var minTemp: Int
     var maxTemp: Int
     var day: String
+    var hours: WeatherHours = WeatherHours()
 }
